b4d19d91074a9553322594440c1e71b7943905ba
Varun-Damarla <damarlavarun2004@gmail.com>
2020-07-06 08:06:01 -0400

    Update Driving.java
    Removed Async functionality to prevent bugs with TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Driving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Driving.java
@@ -9,75 +9,60 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.drive.Robot;
 
-import android.os.AsyncTask;
-
+/**
+ * This is a simple teleop routine for testing localization. Drive the robot around like a normal
+ * teleop routine and make sure the robot's estimated pose matches the robot's actual pose (slight
+ * errors are not out of the ordinary, especially with sudden drive motions). The goal of this
+ * exercise is to ascertain whether the localizer has been configured properly (note: the pure
+ * encoder localizer heading may be significantly off if the track width has not been tuned).
+ */
 
 @Config
-@TeleOp (name = "Driving", group = "00-TeleOp")
-public class Driving extends LinearOpMode{
-
-    Robot robot = new Robot(hardwareMap);
-
+@TeleOp(name = "Driving", group = "00-TeleOp")
+public class Driving extends LinearOpMode {
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
     public static double OMEGA_WEIGHT = 1;
 
     @Override
     public void runOpMode() throws InterruptedException {
-
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        telemetry.addData("Status", "Intialized");
-        telemetry.update();
+        Robot robot = new Robot(hardwareMap);
+
+        waitForStart();
 
         while (opModeIsActive()) {
+            Pose2d baseVel = new Pose2d(
+                    -gamepad1.left_stick_y,
+                    -gamepad1.left_stick_x,
+                    -gamepad1.right_stick_x
+            );
 
-            // Android phone display information
-            telemetry.addData("Status", "Run Time: " + getRuntime());
-            telemetry.update();
-        }
-    }
-
-    // TODO: Work on reimplementing
-    private class AsyncBase extends AsyncTask<Robot, String, String> {
-
-        @Override
-        protected String doInBackground(Robot... params) {
-
-            while (opModeIsActive()) {
-                Pose2d baseVel = new Pose2d(
-                        -gamepad1.left_stick_y,
-                        -gamepad1.left_stick_x,
-                        -gamepad1.right_stick_x
-                );
-
-                Pose2d vel;
-                if (Math.abs(baseVel.getX()) + Math.abs(baseVel.getY()) + Math.abs(baseVel.getHeading()) > 1) {
-                    // re-normalize the powers according to the weights
-                    double denom = VX_WEIGHT * Math.abs(baseVel.getX())
-                            + VY_WEIGHT * Math.abs(baseVel.getY())
-                            + OMEGA_WEIGHT * Math.abs(baseVel.getHeading());
-                    vel = new Pose2d(
-                            VX_WEIGHT * baseVel.getX(),
-                            VY_WEIGHT * baseVel.getY(),
-                            OMEGA_WEIGHT * baseVel.getHeading()
-                    ).div(denom);
-                } else {
-                    vel = baseVel;
-                }
-
-                robot.setDrivePower(vel);
-
-                robot.update();
-
-                Pose2d poseEstimate = robot.getPoseEstimate();
-                telemetry.addData("x", poseEstimate.getX());
-                telemetry.addData("y", poseEstimate.getY());
-                telemetry.addData("heading", poseEstimate.getHeading());
-                telemetry.update();
-
+            Pose2d vel;
+            if (Math.abs(baseVel.getX()) + Math.abs(baseVel.getY()) + Math.abs(baseVel.getHeading()) > 1) {
+                // re-normalize the powers according to the weights
+                double denom = VX_WEIGHT * Math.abs(baseVel.getX())
+                        + VY_WEIGHT * Math.abs(baseVel.getY())
+                        + OMEGA_WEIGHT * Math.abs(baseVel.getHeading());
+                vel = new Pose2d(
+                        VX_WEIGHT * baseVel.getX(),
+                        VY_WEIGHT * baseVel.getY(),
+                        OMEGA_WEIGHT * baseVel.getHeading()
+                ).div(denom);
+            } else {
+                vel = baseVel;
             }
-            return null;
+
+            robot.setDrivePower(vel);
+
+            robot.update();
+
+            Pose2d poseEstimate = robot.getPoseEstimate();
+            telemetry.addData("x", poseEstimate.getX());
+            telemetry.addData("y", poseEstimate.getY());
+            telemetry.addData("heading", poseEstimate.getHeading());
+            telemetry.update();
         }
     }
 }
