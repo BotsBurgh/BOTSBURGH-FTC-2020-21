78d927768f9ac1c3dfbc1f9e42887b8887ac704c
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2020-10-15 20:35:30 -0400

    Update mecanum drive to work with API

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOp/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOp/MecanumDrive.java
@@ -18,25 +18,18 @@ package org.firstinspires.ftc.teamcode.OpMode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.teamcode.API.InitRobot;
+import org.firstinspires.ftc.teamcode.API.Robot;
+
 @TeleOp(name = "Mecanum Drive", group = "Linear OpMode")
-public class SampleMecanumDrive extends LinearOpMode {
+public class MecanumDrive extends LinearOpMode {
+    private static final double MAX_SPEED = 0.8;
     @Override
     public void runOpMode() {
-        // Init Motors
-        DcMotor motorFL = hardwareMap.get(DcMotor.class, "fl");
-        DcMotor motorFR = hardwareMap.get(DcMotor.class, "fr");
-        DcMotor motorBL = hardwareMap.get(DcMotor.class, "bl");
-        DcMotor motorBR = hardwareMap.get(DcMotor.class, "br");
-
-        motorFL.setDirection(DcMotor.Direction.REVERSE);
-        motorBL.setDirection(DcMotor.Direction.REVERSE);
-        motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        InitRobot initializer = new InitRobot(this);
+        Robot robot = initializer.init();
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -51,7 +44,6 @@ public class SampleMecanumDrive extends LinearOpMode {
 
         waitForStart();
         while(opModeIsActive()) {
-
             double x1 = -gamepad1.left_stick_x;
             double y1 = gamepad1.left_stick_y;
             double rotation = -gamepad1.right_stick_x;
@@ -61,15 +53,12 @@ public class SampleMecanumDrive extends LinearOpMode {
             double blPower = Range.clip((y1 - x1 + rotation),-.8,.8);
             double brPower = Range.clip((y1 + x1 - rotation),-.8,.8);
 
-            motorBL.setPower(blPower);
-            motorBR.setPower(brPower);
-            motorFL.setPower(flPower);
-            motorFR.setPower(frPower);
+            robot.getMovement().move4x4(flPower, frPower, blPower, brPower);
 
-            telemetry.addData("Back Left", motorBL.getPower());
-            telemetry.addData("Back Right", motorBR.getPower());
-            telemetry.addData("Front Left", motorFL.getPower());
-            telemetry.addData("Front right", motorFR.getPower());
+            telemetry.addData("Back Left", blPower);
+            telemetry.addData("Back Right", brPower);
+            telemetry.addData("Front Left", flPower);
+            telemetry.addData("Front right", frPower);
             telemetry.update();
         }
     }
