a1e1131aba606b4d3788abdade241ff609a439dd
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2020-10-15 20:22:32 -0400

    Add config/naming, API big 3, and init robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/API/Robot.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020 FIRST Tech Challenge Team 11792
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.firstinspires.ftc.teamcode.API;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.InitRobot;
+import org.firstinspires.ftc.teamcode.API.HW.*;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Integrates Sensor class and Movement class so we can use VuForia and motors in one function.
+ * Use like so: Robot robot = new Robot(new Sensor(whatever), new Movement(whatever));
+ * Refer to Movement and Sensor classes for more information on what those classes do.
+ * NOTE: You really should edit this file to suit your robot. If you find an error occurring here,
+ * add it to our GitHub issues page at https://github.com/botsburgh/BOTSBURGH-FTC-2019-20/issues
+ */
+@Builder
+public class Robot {
+    @Getter private Sensor sensor;
+    @Getter private Movement movement;
+
+    private static final double COUNTS_PER_MOTOR_REV  = 1440; // eg: TETRIX Motor Encoder
+    private static final double DRIVE_GEAR_REDUCTION  = 1.0;  // This is < 1.0 if geared UP
+    private static final double WHEEL_DIAMETER_INCHES = 4.0;  // For figuring circumference
+    private static final double COUNTS_PER_INCH       = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    private static final double HEADING_THRESHOLD = 2.0;  // Any less and our robot acts up
+    private static final double P_TURN_COEFF      = 0.1;  // Larger is more responsive, but also less stable
+    private static final double P_DRIVE_COEFF     = 0.15; // Larger is more responsive, but also less stable
+
+    private static final double TURN_TIMEOUT      = 4.0;  // Timeout to prevent non-stop turning
+    private static final double DRIVE_TIMEOUT     = 10.0; // Maximum execution time for driving
+
+    // Quick and dirty hack to prevent issues with stopping the robot
+    @Getter
+    private LinearOpMode linearOpMode;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitRobot.java
@@ -0,0 +1,178 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+import org.firstinspires.ftc.teamcode.API.HW.*;
+import org.firstinspires.ftc.teamcode.API.Movement;
+import org.firstinspires.ftc.teamcode.API.Robot;
+import org.firstinspires.ftc.teamcode.API.Sensor;
+
+import org.firstinspires.ftc.teamcode.API.Config.Naming;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ * The Robot Initializer. Place initialzation code here. This prevents needing to sync the init code
+ * between all opmodes.
+ */
+public class InitRobot {
+    public static final boolean MODE_4x4 = false; // True if you are using 4x4 drive
+
+    private LinearOpMode l;
+
+    private boolean vuforia;
+
+    public InitRobot(LinearOpMode linearOpMode, Boolean vuforiaEnabled) {
+        this.l = linearOpMode;
+        this.vuforia = vuforiaEnabled;
+    }
+
+    public InitRobot(LinearOpMode linearOpMode) {
+        this(linearOpMode, false);
+    }
+
+    private Robot robot;
+
+    // TODO: JavaDoc
+    public Robot init() {
+        /*
+        * #######                   ######
+        * #       #####  # #####    #     # ###### #       ####  #    #
+        * #       #    # #   #      #     # #      #      #    # #    #
+        * #####   #    # #   #      ######  #####  #      #    # #    #
+        * #       #    # #   #      #     # #      #      #    # # ## #
+        * #       #    # #   #      #     # #      #      #    # ##  ##
+        * ####### #####  #   #      ######  ###### ######  ####  #    #
+        */
+
+        // Get motors
+        SmartMotor bl, br, fl, fr;
+        bl = new SmartMotor(l.hardwareMap.get(DcMotorEx.class, Naming.MOTOR_BL_NAME));
+        br = new SmartMotor(l.hardwareMap.get(DcMotorEx.class, Naming.MOTOR_BR_NAME));
+        if (MODE_4x4) {
+            fl = new SmartMotor(l.hardwareMap.get(DcMotorEx.class, Naming.MOTOR_FL_NAME));
+            fr = new SmartMotor(l.hardwareMap.get(DcMotorEx.class, Naming.MOTOR_FR_NAME));
+        }
+
+        HashMap<String, SmartMotor> motors = new HashMap<>();
+        motors.put(Naming.MOTOR_BL_NAME, bl);
+        motors.put(Naming.MOTOR_BR_NAME, br);
+        if (MODE_4x4) {
+            motors.put(Naming.MOTOR_FL_NAME, fl);
+            motors.put(Naming.MOTOR_FR_NAME, fr);
+        }
+
+        // Get servos
+        //SmartServo grabber = new SmartServo(l.hardwareMap.get(Servo.class, Naming.SERVO_GRABBER_NAME));
+        //SmartServo rotate = new SmartServo(l.hardwareMap.get(Servo.class, Naming.SERVO_ROTATE_NAME));
+        //SmartServo fLeftNew = new SmartServo(l.hardwareMap.get(Servo.class, Naming.SERVO_FOUNDATION_LEFT_NEW_NAME));
+        //SmartServo fRightNew = new SmartServo(l.hardwareMap.get(Servo.class, Naming.SERVO_FOUNDATION_RIGHT_NEW_NAME));
+
+        //fLeftNew.setDirection(Servo.Direction.REVERSE);
+        //fRightNew.setDirection(Servo.Direction.FORWARD);
+
+        // Add servos into the list
+        HashMap<String, SmartServo> servos = new HashMap<>();
+        //servos.put(Naming.SERVO_GRABBER_NAME, grabber);
+        //servos.put(Naming.SERVO_ROTATE_NAME, rotate);
+        //servos.put(Naming.SERVO_FOUNDATION_LEFT_NEW_NAME, fLeftNew);
+        //servos.put(Naming.SERVO_FOUNDATION_RIGHT_NEW_NAME, fRightNew);
+
+        // Get CRServos
+        //CRServo armExtend = l.hardwareMap.get(CRServo.class, Naming.CRSERVO_EXTEND_NAME);
+
+        // Add CRServos into the list
+        HashMap<String, CRServo> crServos = new HashMap<>();
+        //crServos.put(Naming.CRSERVO_EXTEND_NAME, armExtend);
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        bl.setDirection(DcMotor.Direction.REVERSE);
+        br.setDirection(DcMotor.Direction.FORWARD);
+        if (MODE_4x4) {
+            fl.setDirection(DcMotor.Direction.REVERSE);
+            fr.setDirection(DcMotor.Direction.FORWARD);
+        }
+
+        // Set motors to spin in the correct direction
+        //sc.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        if (MODE_4x4) {
+            fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+
+        // Switch direction of servo
+        //rotate.setDirection(Servo.Direction.REVERSE);
+
+        // Get color sensors
+        //ColorSensor scissorDownLimit = l.hardwareMap.get(ColorSensor.class, Naming.COLOR_SENSOR_DOWN_LIMIT_NAME);
+        //ColorSensor scissorUpLimit = l.hardwareMap.get(ColorSensor.class, Naming.COLOR_SENSOR_UP_LIMIT_NAME);
+        //ColorSensor parkSensor = l.hardwareMap.get(ColorSensor.class, Naming.COLOR_SENSOR_PARK);
+
+        // Add color sensors into list
+        HashMap<String, ColorSensor> colorSensors = new HashMap<>();
+        //colorSensors.put(Naming.COLOR_SENSOR_DOWN_LIMIT_NAME, scissorDownLimit);
+        //colorSensors.put(Naming.COLOR_SENSOR_UP_LIMIT_NAME, scissorUpLimit);
+        //colorSensors.put(Naming.COLOR_SENSOR_PARK, parkSensor);
+
+        // Get webcams
+        //WebcamName webcam1 = l.hardwareMap.get(WebcamName.class, Naming.WEBCAME_0_NAME);
+
+        // Add webcams to list
+        HashMap<String, WebcamName> webcams = new HashMap<>();
+        //webcams.put(Naming.WEBCAME_0_NAME, webcam1);
+
+        // Get gyros
+        //BNO055IMU gyro0 = l.hardwareMap.get(BNO055IMU.class, Naming.GYRO_0_NAME);
+        //BNO055IMU gyro1 = l.hardwareMap.get(BNO055IMU.class, Naming.GYRO_1_NAME);
+
+        // Add gyros to list
+        HashMap<String, BNO055IMU> gyros = new HashMap<>();
+        //gyros.put(Naming.GYRO_0_NAME, gyro0);
+        //gyros.put(Naming.GYRO_1_NAME, gyro1);
+
+        // Add lists into the movement class
+        Movement movement = Movement.builder()
+                .motors(motors)
+                .servos(servos)
+                .crServos(crServos)
+                .build();
+
+        // Add lists into sensor class
+        Sensor sensor = Sensor.builder()
+                .colorSensors(colorSensors)
+                .webcams(webcams)
+                .gyros(gyros)
+                .build();
+
+        // Add movement and sensor class into robot class
+        robot = Robot.builder()
+                .sensor(sensor)
+                .movement(movement)
+                .linearOpMode(l)
+                .build();
+
+        // Send power to servos so they don't move
+        for (String key : servos.keySet()) {
+            robot.getMovement().setServo(key, Objects.requireNonNull(servos.get(key)).getPosition());
+        }
+
+        // Initialize gyros
+        for (String key : gyros.keySet()) {
+            robot.getSensor().initGyro(key);
+        }
+
+        return robot;
+    }
+}
+
