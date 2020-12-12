c64d61d9b2d1715f1abb5787e913244b401f1392
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2020-12-11 20:24:48 -0500

    Update odometry wheel distances

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/API/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/API/StandardTrackingWheelLocalizer.java
@@ -32,8 +32,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 2; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 10; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 4; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 15.1; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = 8.3; // in; offset of the lateral wheel
 
     private static Encoder leftEncoder, rightEncoder, lateralEncoder;
 
